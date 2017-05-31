3df61e8817a87fa6d4703514e3e146f17688d11d
Nazarii Muzychuk <muzychuk.n@gmail.com>
2017-05-31 23:40:30 +0300

    Fix code style

--- javascript.js
+++ javascript.js
@@ -1,24 +1,24 @@
-var board = [0, 1, 'X', 3, 4, 5, 'O', 7, 8];
+var board = [0, 1, "X", 3, 4, 5, "O", 7, 8];
 
 var getTable = function (board) {
-    table = document.createElement('table');
-    table.id = 'grid';
+    var table = document.createElement("table");
+    table.id = "grid";
 
-    tbody = document.createElement('tbody');
+    var tbody = document.createElement("tbody");
     table.appendChild(tbody);
 
     for (var i = 0; i < 9; i++) {
-        if (i % 3 == 0) {
-            tr = document.createElement('tr');
+        if (i % 3 === 0) {
+            var tr = document.createElement("tr");
             tbody.appendChild(tr);
         }
 
-        var td = document.createElement('td');
+        var td = document.createElement("td");
         td.onclick = function () {
-            console.log(this);
+            // console.log(this);
         };
 
-        if (board[i] == 'X' || board[i] == 'O') {
+        if (board[i] === "X" || board[i] === "O") {
             td.innerText = board[i];
         }
 
@@ -28,97 +28,27 @@ var getTable = function (board) {
     return table;
 };
 
-var PlayerX = 'X';
-var PlayerO = 'O';
+var PlayerX = "X";
+var PlayerO = "O";
 var currentPlayer = PlayerX;
-var opponent = 'ai';
+var opponent = "ai";
 
-var winner = '';
-
-var startGame = function () {
-    setMessage('Current player is ' + currentPlayer);
-};
+var winner = "";
 
 var setMessage = function (message) {
-    document.getElementById('message').innerText = message;
+    document.getElementById("message").innerText = message;
 };
 
-var makeMoveHuman = function (element) {
-    element.onclick = null;
-    element.innerText = currentPlayer;
-
-    hideOptions();
-
-    if (isEnd()) { return; }
-
-    switchPlayer();
-
-    if (opponent == 'ai') {
-        makeMoverAI();
-    };
+var startGame = function () {
+    setMessage("Current player is " + currentPlayer);
 };
 
-var makeMoverAI = function () {
-    var attempt = 0;
-    var positions = [];
-
-    for (var i = 1; i < 10; i++) {
-        positions.push(i);
-    }
-
-    shuffle(positions);
-
-    for (var i = 0; i < positions.length; i++) {
-        element = document.getElementById('pos' + positions[i]);
-
-        if (!element.innerText) {
-            element.onclick = null;
-            element.innerText = currentPlayer;
-
-            if (isEnd()) { return; }
-
-            switchPlayer();
-            break;
-        }
-    }
-};
-
-function shuffle(array) {
-    var counter = array.length;
-
-    while (counter > 0) {
-        var i = Math.floor(Math.random() * counter);
-        counter--;
-
-        var temp = array[counter];
-        array[counter] = array[i];
-        array[i] = temp;
-    }
-
-    return array;
-}
-
-var isEnd = function () {
-    var elements = document.getElementsByTagName('tr');
-
-    if (isWin(elements)) {
-        winner = currentPlayer;
-        endGame(elements);
-
-        return true;
-    }
-
-    if (isFull(elements)) {
-        endGame(elements);
-
-        return true;
-    }
-
-    return false;
+var hideOptions = function () {
+    var options = document.getElementById("options");
+    options.style.visibility = "hidden";
 };
 
 var isWin = function (elements) {
-
     for (var i = 0; i < 3; i++) {
         // Check rows
         if (elements[i].children[0].innerText &&
@@ -164,6 +94,22 @@ var isWin = function (elements) {
     return false;
 };
 
+var endGame = function (elements) {
+    for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < 3; j++) {
+            elements[i].children[j].onclick = null;
+        }
+    }
+
+    if (winner) {
+        setMessage("The winner is " + winner);
+        document.getElementById("message").style.backgroundColor = "lime";
+    } else {
+        setMessage("Draw");
+        document.getElementById("message").style.backgroundColor = "yellow";
+    }
+};
+
 var isFull = function (elements) {
     for (var i = 0; i < 3; i++) {
         for (var j = 0; j < 3; j++) {
@@ -176,9 +122,23 @@ var isFull = function (elements) {
     return true;
 };
 
-var hideOptions = function () {
-    var options = document.getElementById('options');
-    options.style.visibility = 'hidden';
+var isEnd = function () {
+    var elements = document.getElementsByTagName("tr");
+
+    if (isWin(elements)) {
+        winner = currentPlayer;
+        endGame(elements);
+
+        return true;
+    }
+
+    if (isFull(elements)) {
+        endGame(elements);
+
+        return true;
+    }
+
+    return false;
 };
 
 var switchPlayer = function () {
@@ -188,31 +148,70 @@ var switchPlayer = function () {
         currentPlayer = PlayerX;
     }
 
-    setMessage('Current player is ' + currentPlayer);
-};
-
-var setOpponent = function (selectObject) {
-    opponent = selectObject.value;
-};
-
-var endGame = function (elements) {
-    for (var i = 0; i < 3; i++) {
-        for (var j = 0; j < 3; j++) {
-            elements[i].children[j].onclick = null;
-        }
-    }
-
-    if (winner) {
-        setMessage('The winner is ' + winner);
-        document.getElementById('message').style.backgroundColor = "lime";
-    } else {
-        setMessage('Draw');
-        document.getElementById('message').style.backgroundColor = "yellow";
-    }
+    setMessage("Current player is " + currentPlayer);
 };
 
 var restartGame = function () {
     location.reload();
+};
+
+function shuffle(array) {
+    var counter = array.length;
+
+    while (counter > 0) {
+        var i = Math.floor(Math.random() * counter);
+        counter--;
+
+        var temp = array[counter];
+        array[counter] = array[i];
+        array[i] = temp;
+    }
+
+    return array;
+}
+
+var makeMoverAI = function () {
+    var attempt = 0;
+    var positions = [];
+
+    for (var i = 1; i < 10; i++) {
+        positions.push(i);
+    }
+
+    shuffle(positions);
+
+    for (var j = 0; j < positions.length; j++) {
+        var element = document.getElementById("pos" + positions[j]);
+
+        if (!element.innerText) {
+            element.onclick = null;
+            element.innerText = currentPlayer;
+
+            if (isEnd()) { return; }
+
+            switchPlayer();
+            break;
+        }
+    }
+}
+
+var makeMoveHuman = function (element) {
+    element.onclick = null;
+    element.innerText = currentPlayer;
+
+    hideOptions();
+
+    if (isEnd()) { return; }
+
+    switchPlayer();
+
+    if (opponent === "ai") {
+        makeMoverAI();
+    }
+}
+
+var setOpponent = function (selectObject) {
+    opponent = selectObject.value;
 };
 
 (function () {
